286ee15c6a420e9a84c2a4f830347b0014752ea6
CodeByKarthik <karthinottsuk@gmail.com>
2026-04-21 12:51:43 +0100

    Updated Auth Schema

--- src/five_safes_tes_workbench/schema/auth_schema.py
+++ src/five_safes_tes_workbench/schema/auth_schema.py
@@ -1,20 +1,23 @@
+from typing import Annotated
+from pydantic import AnyHttpUrl, BeforeValidator
+
 from pydantic import BaseModel, model_validator
-from ..enums.auth_enums import AuthMode
-from ..models.auth_dataclass import AuthValidationDataClass
+from ..common.validator_dataclass import AuthValidationDataClass, AuthMode
 
+HttpUrlString = Annotated[str, BeforeValidator(lambda v: str(AnyHttpUrl(v)))]
 
-class AuthConfig(BaseModel):
+class AuthValidationModel(BaseModel):
 
     access_token: str | None = None
 
     submission_keycloak_client_id: str | None = None
     submission_keycloak_client_secret: str | None = None
-    submission_keycloak_url: str | None = None
+    submission_keycloak_url: HttpUrlString | None = None
     submission_keycloak_username: str | None = None
     submission_keycloak_password: str | None = None
 
     @model_validator(mode="after")
-    def validate_auth(self) -> "AuthConfig":
+    def validate_auth(self) -> "AuthValidationModel":
         auth_mode = self._check_auth_mode()
 
         if auth_mode == AuthMode.ACCESS_TOKEN:
@@ -26,7 +29,7 @@ class AuthConfig(BaseModel):
     
 
     def _check_auth_mode(self) -> AuthMode:
-        if self.access_token is not None and self.access_token.strip() != "":
+        if self.access_token is not None:
             return AuthMode.ACCESS_TOKEN
         return AuthMode.CREDENTIALS
     
@@ -36,6 +39,7 @@ class AuthConfig(BaseModel):
             raise ValueError(
                 "Access token must not be empty for ACCESS_TOKEN auth mode."
             )
+        self.access_token = self.access_token.strip()
         
 
     def _validate_credentials(self) -> None:
