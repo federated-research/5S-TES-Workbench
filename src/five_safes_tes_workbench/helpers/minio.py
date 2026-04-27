fae96f4ff06a9a410e933d48e3a2b83b43431a52
AndrewThien <anretrithien@gmail.com>
2026-04-27 12:19:51 +0100

    get and check status of child

--- src/five_safes_tes_workbench/helpers/minio.py
+++ src/five_safes_tes_workbench/helpers/minio.py
@@ -1,15 +1,26 @@
 import csv
 import json
+from dataclasses import dataclass
 from typing import Any
 from io import StringIO
 from minio import Minio
 import minio
 import requests
+from five_safes_tes_workbench.constants.task_status import (
+    TASK_STATUS_DESCRIPTIONS,
+    TaskStatus,
+)
 from ..schema.config_schema import ConfigValidationModel
 from urllib.parse import urlparse
 from ..utils.logger import get_logger
 
 logger = get_logger(__name__)
+
+
+@dataclass
+class ChildTaskInfo:
+    id: str
+    status: TaskStatus
 
 
 def require_client(client: Minio | None) -> Minio:
@@ -38,14 +49,14 @@ def is_https(url: str) -> bool:
     raise ValueError(f"URL must start with http:// or https://, got: {url!r}")
 
 
-def get_child_task_id(
+def get_child_task_info(
     config: ConfigValidationModel, parent_task_id: str, tre: str
-) -> str:
+) -> ChildTaskInfo:
     """
     Get the child task ID for a given task and TRE.
     """
     response = requests.get(
-        f"{config.tes_base_url.rstrip('/')}/api/Submission/GetChildSubmissionIdByParentAndTre?parentSubmissionId={parent_task_id}&treName={tre}"
+        f"{config.tes_base_url.rstrip('/')}/api/Submission/GetChildSubmissionInfoByParentAndTre?parentSubmissionId={parent_task_id}&treName={tre}"
     )
     response.raise_for_status()
     if response.status_code != 200:
@@ -53,13 +64,46 @@ def get_child_task_id(
             f"Failed to get child task ID: {response.status_code} {response.text}"
         )
 
-    child_task_id = response.json()
-    if child_task_id is None:
+    child_task_info = response.json()
+    if child_task_info is None:
         raise RuntimeError(
             f"No child task ID found for parent task {parent_task_id} and TRE {tre}"
         )
-    logger.info("Child task ID: %s", child_task_id)
-    return child_task_id
+    logger.info(
+        "Child task ID: %s, status: %s",
+        child_task_info["id"],
+        TASK_STATUS_DESCRIPTIONS[TaskStatus(child_task_info["status"])],
+    )
+    return ChildTaskInfo(
+        id=child_task_info["id"],
+        status=TaskStatus(child_task_info["status"]),
+    )
+
+
+def check_child_task_status(child_task_info: ChildTaskInfo) -> str | None:
+    """
+    Check the status of a child task and return an error message if the task is not completed.
+
+    Parameters
+    ----------
+    - child_task_info: The child task information.
+
+    Returns
+    -------
+    An error message if the task is not completed, failed or cancelled, otherwise None.
+    """
+    if (
+        child_task_info.status == TaskStatus.FAILED
+        or child_task_info.status == TaskStatus.CANCELLED
+        or child_task_info.status == TaskStatus.FAILURE
+    ):
+        return (
+            f"Child task {child_task_info.id} is failed or cancelled. Please try again."
+        )
+
+    elif child_task_info.status != TaskStatus.COMPLETED:
+        return f"Child task {child_task_info.id} is not completed, so results are not available yet. Please wait for the task to complete and try again. Current status: {TASK_STATUS_DESCRIPTIONS[TaskStatus(child_task_info.status)]}"
+    return None
 
 
 def list_results(
