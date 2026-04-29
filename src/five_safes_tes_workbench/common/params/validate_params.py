b718908096961411fefc86fd67b984f857908174
CodeByKarthik <karthinottsuk@gmail.com>
2026-04-29 12:05:28 +0100

    Updated Validate params

--- src/five_safes_tes_workbench/common/params/validate_params.py
+++ src/five_safes_tes_workbench/common/params/validate_params.py
@@ -5,7 +5,7 @@ from ..enums.validator_enums import AuthParamEnums, ConfigParamEnums
 
 class ConfigValidationParams(TypedDict, total=False):
     """
-    TypedDict for validation parameters to 
+    TypedDict for validation parameters to
     be passed as keyword arguments to
     `Workbench.validate()`.
 
@@ -16,6 +16,7 @@ class ConfigValidationParams(TypedDict, total=False):
         2. Auth parameters: Optional parameters
            for authentication.
     """
+
     # --- Config parameters ---
 
     project: Required[str]
@@ -35,7 +36,9 @@ class ConfigValidationParams(TypedDict, total=False):
     password: str
 
 
-def split_config_params(params: ConfigValidationParams) -> tuple[dict[str, object], dict[str, object]]:
+def split_config_params(
+    params: ConfigValidationParams,
+) -> tuple[dict[str, object], dict[str, object]]:
     """Splits the input parameters into config and auth
     parameters based on predefined keys.
 
@@ -52,20 +55,6 @@ def split_config_params(params: ConfigValidationParams) -> tuple[dict[str, objec
     auth = {k: v for k, v in params.items() if k in _AUTH_KEYS}
     return config, auth
 
-_CONFIG_KEYS = {
-    ConfigParamEnums.PROJECT.value,
-    ConfigParamEnums.TES_BASE_URL.value,
-    ConfigParamEnums.MINIO_STS_ENDPOINT.value,
-    ConfigParamEnums.MINIO_ENDPOINT.value,
-    ConfigParamEnums.MINIO_OUTPUT_BUCKET.value,
-    ConfigParamEnums.TRES.value,
-}
 
-_AUTH_KEYS = {
-    AuthParamEnums.ACCESS_TOKEN.value,
-    AuthParamEnums.CLIENT_ID.value,
-    AuthParamEnums.CLIENT_SECRET.value,
-    AuthParamEnums.KEYCLOAK_URL.value,
-    AuthParamEnums.USERNAME.value,
-    AuthParamEnums.PASSWORD.value,
-}
+_CONFIG_KEYS = {e.value for e in ConfigParamEnums}
+_AUTH_KEYS = {e.value for e in AuthParamEnums}
