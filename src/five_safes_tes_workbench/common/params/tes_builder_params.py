772cb1385cce69234414575ab57093db665edf28
CodeByKarthik <karthinottsuk@gmail.com>
2026-04-30 14:44:54 +0100

    Updated TES Builder Params

--- src/five_safes_tes_workbench/common/params/tes_builder_params.py
+++ src/five_safes_tes_workbench/common/params/tes_builder_params.py
@@ -1,24 +1,22 @@
-from typing import NotRequired, Required, TypedDict
+from typing import Required, TypedDict
 
 
 class TESTaskParams(TypedDict):
     """
-    Parameters for building a TES task.
+    Parameters for building a TES task and
+    all fields are required.
 
-    Required:
-        - image: Container image to run.
-        - command: Command to execute in the container.
-
-    Optional:
-        - name: Task name.
-        - description: Task description.
-        - output_url: S3 output URL.
-        - output_path: Output path inside the container.
+    - image: Container image to run.
+    - command: Command to execute in the container.
+    - name: Task name.
+    - description: Task description.
+    - output_url: S3 output URL.
+    - output_path: Output path inside the container.
     """
 
     image: Required[str]
     command: Required[list[str]]
     name: Required[str]
-    description: NotRequired[str]
-    output_url: NotRequired[str]
-    output_path: NotRequired[str]
+    description: Required[str]
+    output_url: Required[str]
+    output_path: Required[str]
