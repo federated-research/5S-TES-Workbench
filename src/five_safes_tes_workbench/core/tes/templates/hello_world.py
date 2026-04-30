a3c7c91d07bd90031085bd9b386de45f9725a1df
CodeByKarthik <karthinottsuk@gmail.com>
2026-04-30 16:43:12 +0100

    Update Hello World Template

--- src/five_safes_tes_workbench/core/tes/templates/hello_world.py
+++ src/five_safes_tes_workbench/core/tes/templates/hello_world.py
@@ -1,5 +1,9 @@
 from ....common.params.helloworld_params import HelloWorldUserParams
-from ....common.params.tes_builder_params import TESTaskParams
+from ....common.params.tes_builder_params import (
+    ExecutorTESParams,
+    OutputTESParams,
+    TESTaskParams,
+)
 from ...base.tes_template_abc import BaseTESTemplate
 
 
@@ -8,36 +12,39 @@ class HelloWorldTemplate(BaseTESTemplate[HelloWorldUserParams]):
     "Hello World" template for TES tasks.
 
     Method Usage:
-    - `build_tes.hello_world(name=..., description=..., image=...)`
+    - `build_tes.hello_world(name=..., description=..., image=..., command=...)`
     """
 
     @property
     def name(self) -> str:
-        """
-        Unique name for this template.
-        """
+        """Unique name for this template."""
         return "hello_world"
 
     def resolve(self, overrides: HelloWorldUserParams) -> TESTaskParams:
-        """
-        Merges user-provided params with internal defaults.
-        """
-
-        # ---- Fixed parameters for this template ----
+        """Merges user-provided params with internal defaults."""
 
         _NAME = overrides.get("name", "Hello World Task")
+        _DESCRIPTION = overrides.get("description", "Hello World")
         _IMAGE = overrides.get("image", "alpine:latest")
         _COMMAND = overrides.get("command", ["echo", "hello world"])
-        _DESCRIPTION = overrides.get("description", "Hello World")
         _OUTPUT_URL = overrides.get("output_url", "s3://hello-world-output")
         _OUTPUT_PATH = overrides.get("output_path", "/outputs")
 
         return TESTaskParams(
-
             name=_NAME,
-            image=_IMAGE,
-            command=_COMMAND,
             description=_DESCRIPTION,
-            output_url=_OUTPUT_URL,
-            output_path=_OUTPUT_PATH,
+            executors=[
+                ExecutorTESParams(
+                    executor_image=_IMAGE,
+                    executor_command=_COMMAND,
+                )
+            ],
+            outputs=[
+                OutputTESParams(
+                    output_name="Stdout",
+                    output_description="Stdout results",
+                    output_url=_OUTPUT_URL,
+                    output_path=_OUTPUT_PATH,
+                )
+            ],
         )
